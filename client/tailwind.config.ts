543bd9cd612c43fcbbaf043083ae431a18f70ef3
Hugo <h.pereirapio@gmail.com>
2024-01-18 16:14:41 +0100

    Update tailwind config to work with ts and tsx files

---
+++ client/tailwind.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#171717",
+      },
+    },
+  },
+  plugins: [],
+};
