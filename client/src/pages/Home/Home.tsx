245b6a8e682c451fdac9513454278c501aa7007e
Hugo <h.pereirapio@gmail.com>
2024-02-02 18:24:16 +0100

    Add Banner and shadcn config

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,24 @@
+import { Banner } from "@/components/Banner";
+import { useEffect, useState } from "react";
+
 export const Home = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const userAgent =
+      typeof window.navigator === "undefined" ? "" : navigator.userAgent;
+    const mobile = Boolean(
+      userAgent.match(
+        /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i,
+      ),
+    );
+    setIsMobile(mobile);
+  }, []);
+
   return (
     <div>
       <h1>Home</h1>
+      {!isMobile && <Banner />}
     </div>
   );
 };
