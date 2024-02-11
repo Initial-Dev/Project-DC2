305a0707452fe7eec9b35cbf9c94d40efce498aa
Louis Bellefemine <bellefemine.louis@gmail.com>
2024-02-11 19:03:32 +0100

    api v1 done with postman collection

--- api/Cart/src/controllers/CartController.ts
+++ api/Cart/src/controllers/CartController.ts
@@ -1,10 +1,81 @@
-import { Request, Response } from 'express';
-import {getCartItems, addItemToCart} from "../services";
+import {Request, Response} from 'express';
+import {
+  addItem,
+  changeItemQuantity,
+  cleanCart,
+  getCartItems,
+  getTotalPriceForUser,
+  goToPayment,
+  removeItem
+} from "../services";
 
-export async function getCart(req: Request, res: Response) {
-    res.send(await getCartItems());
+export async function getCartDetails(req: Request<{ idUser: number }>, res: Response) {
+  try {
+    if (!req.params.idUser) res.status(400).send("User ID is required");
+
+    return res.status(200).send(await getCartItems(Number(req.params.idUser)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 }
 
-export async function addToCart(req: Request, res: Response) {
-    res.send(await addItemToCart());
+export async function addItemToCart(req: Request<{ idItem: number, idUser: number, quantity: number }>, res: Response) {
+  try {
+    if (!req.body.idItem || !req.body.idUser || !req.body.quantity) res.status(400).send("Item ID, User ID and quantity are required");
+
+    return res.status(201).send(await addItem(Number(req.body.idUser), Number(req.body.idItem), Number(req.body.quantity)));
+
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function updateItemQuantity(req: Request<{ idItem: number, idUser: number }>, res: Response) {
+  try {
+    if (!req.body.idItem || !req.body.idUser || !req.body.quantity) res.status(400).send("Item ID, User ID and quantity are required");
+
+    return res.status(200).send(await changeItemQuantity(Number(req.body.idUser), Number(req.body.idItem), Number(req.body.quantity)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function removeItemFromCart(req: Request<{ idItem: number, idUser: number }>, res: Response) {
+  try {
+    if (!req.body.idItem || !req.body.idUser) res.status(400).send("Item ID and User ID are required");
+
+    res.status(200).send(await removeItem(Number(req.body.idUser), Number(req.body.idItem)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function emptyCart(req: Request<{ idUser: number }>, res: Response) {
+  try {
+    if (!req.body.idUser) res.status(400).send("User ID is required");
+
+    res.status(200).send(await cleanCart(Number(req.body.idUser)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getCartTotalPrice(req: Request<{ idUser: number }>, res: Response) {
+  try {
+    if (!req.params.idUser) res.status(400).send("User ID is required");
+
+    res.status(200).send(await getTotalPriceForUser(Number(req.params.idUser)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function proceedToPayment(req: Request<{ idUser: number }>, res: Response) {
+  try {
+    if (!req.body.idUser) res.status(400).send("User ID is required");
+
+    return res.status(200).send(await goToPayment(Number(req.body.idUser)));
+  } catch (error: any) {
+    res.status(500).send(error.message);
+  }
 }
--- api/Cart/src/services/CartServices.ts
+++ api/Cart/src/services/CartServices.ts
@@ -2,20 +2,61 @@ import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function getCartItems() {
-  return prisma.cart.findFirst({
-      where: {
-          userID: 1,
-      },
+export async function getCartItems(idUser: number) {
+  return prisma.cart.findMany({
+    where: {
+      userID: idUser,
+    },
   });
 }
 
-export async function addItemToCart() {
+export async function addItem(idUser: number, idItem: number, quantity: number) {
   return prisma.cart.create({
-      data: {
-          userID: 1,
-          itemId: 1,
-          quantity: 1,
-      },
+    data: {
+      userID: idUser,
+      itemId: idItem,
+      quantity: quantity,
+    },
   });
+}
+
+export async function changeItemQuantity(idUser: number, idItem: number, quantity: number) {
+  return prisma.cart.update({
+    where: {
+      userID_itemId: {
+        userID: idUser,
+        itemId: idItem,
+      },
+    },
+    data: {
+      quantity: quantity,
+    },
+  });
+}
+
+export async function removeItem(idUser: number, idItem: number) {
+  return prisma.cart.delete({
+    where: {
+      userID_itemId: {
+        userID: idUser,
+        itemId: idItem,
+      },
+    },
+  });
+}
+
+export async function cleanCart(idUser: number) {
+  return prisma.cart.deleteMany({
+    where: {
+      userID: idUser,
+    },
+  });
+}
+
+export async function getTotalPriceForUser(idUser: number) {
+  return `TODO: Get user ${idUser} cart total price`;
+}
+
+export async function goToPayment(idUser: number) {
+  return `TODO: Proceed to user ${idUser} payment`;
 }
