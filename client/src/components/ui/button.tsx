b3b0f6fb0475db2ee29c582e8e7d0b86b12047fd
Amaury VANDENBUSSCHE <asurafujiwara06@gmail.com>
2024-02-13 19:18:44 +0100

    fixed carousel + ui small additions

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -1,8 +1,10 @@
+// components/ui/button.js
+
 import { Slot } from "@radix-ui/react-slot";
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
-import { buttonVariants, ButtonVariantsProps } from "./buttonVariants"; // Assurez-vous que le chemin est correct
+import { buttonVariants, ButtonVariantsProps } from "./buttonVariants";
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -12,20 +14,31 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, link, size, asChild = false, ...props }, ref) => {
+  (
+    { className, variant, link, size, asChild = false, onClick, ...props },
+    ref,
+  ) => {
     const Comp = asChild ? Slot : "button";
+
+    const handleClick = () => {
+      if (link) {
+        window.location.href = link;
+      } else if (onClick) {
+        onClick();
+      }
+    };
+
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-        onClick={() => {
-          if (link) window.location.href = link;
-        }}
+        onClick={handleClick}
       />
     );
   },
 );
+
 Button.displayName = "Button";
 
 export { Button };
