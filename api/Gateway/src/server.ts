0958b8ab3ef43f684c1e699b2d8c9a484d064c66
Hugo PEREIRA PIO <109863561+hugo-prp@users.noreply.github.com>
2024-02-14 12:57:31 +0000

    add product api to client

--- api/Gateway/src/server.ts
+++ api/Gateway/src/server.ts
@@ -1,18 +1,18 @@
-import ExpressConfig from "./config/express.config"
-import morgan from "morgan";
-import ROUTES from "./routes/routes";
-import setupProxies from "./config/proxy";
-import cors from "cors";
+import cors from 'cors';
+import morgan from 'morgan';
+import ExpressConfig from './config/express.config';
+import setupProxies from './config/proxy';
+import ROUTES from './routes/routes';
 
-const app = ExpressConfig()
-const PORT = process.env.PORT || 5000
+const app = ExpressConfig();
+const PORT = process.env.PORT || 5000;
 
-app.use(morgan("combined"))
-app.use(cors())
+app.use(morgan('combined'));
+app.use(cors());
 setupProxies(app, ROUTES);
 
-app.listen(PORT, () => console.log("Server Running on Port: " + PORT))
+app.listen(PORT, () => console.log('Server Running on Port: ' + PORT));
 
 app.get('/', (_req, res) => {
-  res.send('Gateway OK !')
-})
+	res.send('Gateway OK !');
+});
